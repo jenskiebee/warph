1c002703667ef03bda6eb62a26d6951e06796f96
jenskiebee <37134406+jenskiebee@users.noreply.github.com>
2019-11-18 12:22:02 +0800

    Update warph.bash
    okay

--- warph.bash
+++ warph.bash
@@ -23,7 +23,7 @@ cd storage/shared
 rm -rf WireGuard && mkdir -p WireGuard
 cd WireGuard
 curl -4sO 'https://raw.githubusercontent.com/jenskiebee/warph/master/wgconfig.py'
-python wgcf.py
+python wgconfig.py
 cp wgcf-profile.conf ImportThis.conf
 clear
 echo -e "\n\nYour Cloudflare WARP client is Successfully Configured\nYour config is located at /storage/emulated/0/WireGuard/ImportThis.conf\nNow Open your WireGuard App then click the plus sign and then tap/click the 'Create from file or Archive'\nThen locate the WireGuard folder on your phone internal storage and click or tap the ImportThis.conf file\nAnd lastly tap/click the switch to run the VPN\n\n Enjoy Surfing!"
