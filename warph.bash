1e85b00badcdd13a5f95100c3ff8745017922008
jenskiebee <37134406+jenskiebee@users.noreply.github.com>
2019-11-18 12:46:10 +0800

    Update warph.bash

--- warph.bash
+++ warph.bash
@@ -1,7 +1,8 @@
 #!/bin/bash
 # CloudFlare WARP Client Config generator for WireGuard
-# Script by jenskie
+# Script by Bon-chan
 clear
+
 if [ "$(uname -o)" != 'Android' ]; then
  echo -e "[\e[1;31mError\e[0m] This script is for Termux Android users only"
  exit 1
@@ -13,7 +14,6 @@ termux-setup-storage
 echo -e "To continue configuring, press \e[1;32mAllow\e[0m\n"
 sleep 3
 
-
 cd
 apt update && apt upgrade -y
 apt install python wireguard-tools git nano curl wget -y
@@ -22,9 +22,9 @@ pip install requests
 cd storage/shared
 rm -rf WireGuard && mkdir -p WireGuard
 cd WireGuard
-curl -4sO 'https://raw.githubusercontent.com/jenskiebee/warph/master/wgconfig.py'
-python wgconfig.py
-cp wgcon.conf ImportThisNewFile.conf
+curl -4sO 'https://raw.githubusercontent.com/Bonveio/cloudflare-warp-wgclient-bash/master/wgcf.py'
+python wgcf.py
+cp wgcf-profile.conf ImportThis.conf
 clear
 echo -e "\n\nYour Cloudflare WARP client is Successfully Configured\nYour config is located at /storage/emulated/0/WireGuard/ImportThis.conf\nNow Open your WireGuard App then click the plus sign and then tap/click the 'Create from file or Archive'\nThen locate the WireGuard folder on your phone internal storage and click or tap the ImportThis.conf file\nAnd lastly tap/click the switch to run the VPN\n\n Enjoy Surfing!"
 cd
